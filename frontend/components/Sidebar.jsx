0af4be0371a239a69b82870e900bf57560cf7d5b
Clifford Tse <TseClifford@gmail.com>
2022-04-11 14:09:50 -0400

    add sidebar icons

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -1,12 +1,24 @@
+import Link from "next/link";
+import {
+  Summarize,
+  SyncAlt,
+  AccountBox,
+  FolderSpecial,
+} from "@mui/icons-material";
+
 const Sidebar = () => {
   return (
-    <div className="flex flex-col justify-between px-5">
-      <button>Homepage</button>
-      <div className="flex flex-col justify-center content-center w-120 h-full space-y-10">
-        <button>Summary</button>
-        <button>Transfer</button>
-        <button>Personal</button>
-        <button>Summary</button>
+    <div className="flex flex-col justify-between items-center px-5">
+      <Link href="/">App Icon</Link>
+      <div className="flex flex-col justify-center items-center w-120 h-full space-y-10">
+        <Summarize onClick={() => console.log("Summary")} />
+        Summary
+        <SyncAlt onClick={() => console.log("Transfer")} />
+        Transfer
+        <AccountBox onClick={() => console.log("Account")} />
+        Account
+        <FolderSpecial onClick={() => console.log("Portfolio")} />
+        Portfolio
       </div>
       <button>Settings</button>
     </div>
