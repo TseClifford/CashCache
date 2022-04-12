723080acdd52c0e7269443ecd0d9e05c3793a9b6
Clifford Tse <TseClifford@gmail.com>
2022-04-12 14:22:26 -0400

    finish BalanceBox component styling and basic logic

---
+++ frontend/components/WindowElements/BalanceBox.jsx
@@ -0,0 +1,33 @@
+import { Box } from "@mui/material";
+import { ArrowDropUp, ArrowDropDown } from "@mui/icons-material";
+
+const BalanceBox = () => {
+  const dailyPrice = 3.14;
+  let priceColor = "white";
+  let priceMovement;
+
+  if (dailyPrice > 0) {
+    priceColor = "green";
+    priceMovement = <ArrowDropUp />;
+  }
+  if (dailyPrice < 0) {
+    priceColor = "red";
+    priceMovement = <ArrowDropDown />;
+  }
+
+  return (
+    <Box className="flex flex-col justify-between w-1/2 p-5 bg-white rounded-3xl space-y-5 overflow-clip">
+      <p className="font-bold">Your Total Balance</p>
+      <p className="text-5xl font-bold text-purple-800">$123,000</p>
+      <p className="text-slate-400">April 12, 2022 • 12:19 PM</p>
+      <Box
+        className={`flex flex-row justify-center items-center self-center w-1/3 h-4 p-5 rounded-xl bg-${priceColor}-100 text-${priceColor}-600`}
+      >
+        {priceMovement ? priceMovement : null}
+        {dailyPrice !== 0 ? <p>{dailyPrice}%</p> : null}
+      </Box>
+    </Box>
+  );
+};
+
+export default BalanceBox;
--- frontend/pages/summary.js
+++ frontend/pages/summary.js
@@ -1,10 +1,24 @@
+import BalanceBox from "../components/WindowElements/BalanceBox";
+
 export default function Summary() {
   return (
     <div className="flex flex-row w-full">
-      <div className="flex w-1/2 justify-center text-center px-5 py-10">
-        Page Info
-      </div>
-      <div className="flex w-1/2 justify-center text-center px-5 py-10">
+      <main className="flex flex-col w-2/3 text-center px-5 py-10 bg-yellow-100 space-y-10">
+        <section className="flex flex-row justify-between items-center">
+          <div>
+            <p>CashCache Summary</p>
+            <p>Good Evening, Clifford!</p>
+          </div>
+          <div>Search Bar</div>
+        </section>
+        <section className="flex flex-row justify-between">
+          <BalanceBox />
+          <div>Send Money</div>
+        </section>
+        <div>Payment Chart/Graph</div>
+        <div>Invoice History</div>
+      </main>
+      <div className="flex w-1/3 justify-center text-center px-5 py-10 bg-red-100">
         Profile Summary
       </div>
     </div>
