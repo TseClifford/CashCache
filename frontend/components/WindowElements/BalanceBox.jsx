72f954b87dd6ab533055c697c6479fd114f2f9da
Clifford Tse <TseClifford@gmail.com>
2022-04-12 15:16:14 -0400

    fix BalanceBox styling bugs

--- frontend/components/WindowElements/BalanceBox.jsx
+++ frontend/components/WindowElements/BalanceBox.jsx
@@ -18,10 +18,12 @@ const BalanceBox = () => {
   return (
     <Box className="flex flex-col justify-between w-1/2 p-5 bg-white rounded-3xl space-y-5 overflow-clip">
       <p className="font-bold">Your Total Balance</p>
-      <p className="text-5xl font-bold text-purple-800">$123,000</p>
+      <p className="sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-purple-800">
+        $123,000
+      </p>
       <p className="text-slate-400">April 12, 2022 • 12:19 PM</p>
       <Box
-        className={`flex flex-row justify-center items-center self-center w-1/3 h-4 p-5 rounded-xl bg-${priceColor}-100 text-${priceColor}-600`}
+        className={`flex flex-row justify-center items-center self-center h-4 p-5 rounded-xl bg-${priceColor}-100 text-${priceColor}-600`}
       >
         {priceMovement ? priceMovement : null}
         {dailyPrice !== 0 ? <p>{dailyPrice}%</p> : null}
