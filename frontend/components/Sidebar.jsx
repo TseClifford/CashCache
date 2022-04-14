a08943120357f7b197a6bfc2b9940176e64ac160
Clifford Tse <TseClifford@gmail.com>
2022-04-14 18:59:03 -0400

    add component section borders, update sidebar icon hover styling

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import LinkedIcon from "./LinkedIcon";
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col justify-between items-center w-32 px-5 py-10">
+    <div className="flex flex-col justify-between items-center w-28 p-10 border-r-2 border-slate-100">
       <Link href="/">App Icon</Link>
       <div className="flex flex-col justify-center items-center h-full space-y-10">
         <LinkedIcon feature={"Summary"} />
--- frontend/pages/summary.js
+++ frontend/pages/summary.js
@@ -6,7 +6,7 @@ import SearchBar from "../components/WindowElements/SearchBar";
 export default function Summary() {
   return (
     <div className="flex flex-row w-full">
-      <main className="flex flex-col w-2/3 text-center px-5 py-10 space-y-10">
+      <main className="flex flex-col w-2/3 text-center p-10 space-y-10 border-r-2 border-slate-100">
         <section className="flex flex-row justify-between items-center">
           <div className="text-left">
             <p className="text-2xl">
@@ -23,9 +23,8 @@ export default function Summary() {
           <MoneyTransfer />
         </section>
         <SpendingGraph />
-        <div>Invoice History</div>
       </main>
-      <div className="flex w-1/3 justify-center text-center px-5 py-10 bg-red-100">
+      <div className="flex w-1/3 justify-center text-center p-10">
         Profile Summary
       </div>
     </div>
